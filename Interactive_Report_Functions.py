bd09d3bfba62a7b54e51b5c34a9dacab0a805334
Henrique Meira de Oliveira <46904641+oliveirahenrique70@users.noreply.github.com>
2021-04-28 12:02:56 -0300

    Update Interactive_Report_Functions.py

--- Interactive_Report_Functions.py
+++ Interactive_Report_Functions.py
@@ -1,18 +1,13 @@
 ### Truss Interactive Report Functions ###
-### March 2021 ###
+### April 2021 ###
 
 # Import packages
-import os
 import glob
 import pandas as pd
-import datetime as dt
 import ipywidgets as widgets
 
 # 1. Function to get trades
 def Get_Trades(x):
-
-    # Change working directory
-    os.chdir(x)
 
     # Get files names
     all_files = glob.glob('*.csv')
@@ -51,31 +46,3 @@ def Button_Clean():
     
     # Return output
     return(output)
-
-# 6. Refine BR receipt
-def Refine_BR_receipt(x):
-
-    # Fill missing values 
-    x['FX Description'].fillna('-', inplace=True)
-    x['Valor dos Negocios'].fillna('-', inplace=True)
-    x['Comm'].fillna('-', inplace=True)
-    x['Exc Fee'].fillna('-', inplace=True)
-    x = x.fillna(0)
-
-    # Create Nota BM&F cumulative sum
-    x['Nota BM&F Cum Sum'] = x['Nota BM&F'].cumsum()
-    x['FX Value Cum Sum'] = x['FX Value'].cumsum()
-
-    # Create Settlement cumulative sum
-    x['Settlement Cum Sum'] = x['Nota BM&F'].shift(1)
-    x['Settlement Cum Sum'] = x['Settlement Cum Sum'].fillna(0) + x['FX Value']
-    x['Settlement Cum Sum'] = x['Settlement Cum Sum'].cumsum()
-
-    # Create total vector
-    x['Total Cum Sum'] = x['Nota BM&F'] + x['FX Value']
-
-    # Apply cumulative sum to total vector
-    x['Total Cum Sum'] = x['Total Cum Sum'].cumsum()
-    
-    # Return output
-    return(x)
